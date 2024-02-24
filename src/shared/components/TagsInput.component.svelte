7c0f15afaf1f843f3b85e822cf8f2bd21c6a19fe
Robert Goniszewski <robertgoniszewski@outlook.com>
2024-02-24 20:52:05 +0100

    feat: add TagsInput component
    Signed-off-by: Robert Goniszewski <robertgoniszewski@outlook.com>

---
+++ src/shared/components/TagsInput.component.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import MultiSelect from 'svelte-multiselect/dist/MultiSelect.svelte';
+	import { currentTab } from '~shared/stores';
+
+	export let fetchedTags;
+	export let selectedTags;
+</script>
+
+<MultiSelect
+	options={fetchedTags}
+	allowUserOptions={true}
+	duplicates={false}
+	bind:selected={$selectedTags.tags}
+/>
