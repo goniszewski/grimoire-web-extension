c48450e51377557b47c672392d58b78ace2ae2ec
Robert Goniszewski <robertgoniszewski@outlook.com>
2024-10-08 22:03:22 +0200

    chore: update dependencies
    Signed-off-by: Robert Goniszewski <robertgoniszewski@outlook.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  darkMode: "class",
-  content: ["./**/*.svelte"],
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	mode: 'jit',
+	darkMode: 'class',
+	content: ['./src/**/*.svelte'],
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
 		themes: ['fantasy', 'dracula']
 	}
-}
-
+};
